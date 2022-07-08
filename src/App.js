2a72263c43e2cde912a7344ed8fd74330b0e9c52
Melis Augsburger <melis.augsburger@outlook.com>
2022-07-08 10:13:06 +0200

    Searchbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Searchbar from './Searchbar';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Searchbar />
     </div>
   );
 }
---
+++ src/Searchbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Searchbar(){
+    return(
+        <input className="searchbar" type="input" placeholder='Enter keyword or IP address'></input>
+    )
+}
